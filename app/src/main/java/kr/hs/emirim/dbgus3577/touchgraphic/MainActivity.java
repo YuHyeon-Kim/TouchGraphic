4d186b7a1ac285f6cac83f23925f5080ec37cc43
YuHyeon-Kim <dbgus3577@gmail.com>
2016-09-29 20:10:10 +0900

    #4 Add View and Edit Handler method

--- app/src/main/java/kr/hs/emirim/dbgus3577/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbgus3577/touchgraphic/MainActivity.java
@@ -1,6 +1,3 @@
-
-package kr.hs.emirim.dbgus3577.touchgraphic;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,16 +5,30 @@ import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int LINE=1, RECT=2, CIRCLE=3;
+    DrawShape ds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawShape(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+        ds = new DrawShape(getApplicationContext());
+        LinearLayout linear = (LinearLayout)findViewById(R.id.linear_view);
+        linear.addView(ds);
     }
     public void drawShape(View v) {
-
+        switch(v.getId()){
+            case R.id.but_line:
+                break;
+            case R.id.but_rect:
+                break;
+            case R.id.but_circle:
+                break;
+        }
     }
     class DrawShape extends View{
         DrawShape(Context context){
